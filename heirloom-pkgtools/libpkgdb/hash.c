f7ce4e24a4210a525555596bf42907750d209b2e
gritter <gritter>
2007-02-27 20:13:29 +0000

    removed artifacts resulting from CVS import

--- heirloom-pkgtools/libpkgdb/hash.c
+++ heirloom-pkgtools/libpkgdb/hash.c
@@ -12,7 +12,7 @@
 ** This is the implementation of generic hash-tables
 ** used in SQLite.
 **
-** $Id: hash.c,v 1.10 2003/05/12 23:06:53 drh Exp $
+** from Id: hash.c,v 1.10 2003/05/12 23:06:53 drh Exp
 */
 #include "sqliteInt.h"
 #include <assert.h>
